d6af836991d15d7a76a8b888d475db2830531e71
Isrrael Chavez <junior_nachos@hotmail.com>
2021-11-05 08:19:58 -0600

    Cleaned up parts of code, increased size of array

--- my sorting alg/Source.cpp
+++ my sorting alg/Source.cpp
@@ -8,16 +8,22 @@
 
 #include<iostream>
 #include<array>//needed for the size function
-#include "Source.h"
+#include <ctime>
+
 
 using namespace std;
 
-void sortArray(int  values[], int& buffer, int arraysize);
+void sortArray(int  values[], int arraysize);
 
-int main(int argc, char** argv[]) {//these parameters inside main seem to be used for running a program from a command terminal and using main as if it were a custom function
-	int values[] = { 4, 6, 2, 8, 34, 1, 4, 2, 9, 74, 0 };//11 numbers in array
+int main(int argc, char* argv[]) {//these parameters inside main seem to be used for running a program from a command terminal and using main as if it were a custom function
+	srand(time(NULL));
+	int values[100];
 	int arraysize = size(values);
-	int buffer;//integer used for temporarily storing a value of a number in the array
+	for (int i = 0; i < arraysize; i++) {
+		values[i] = rand() % 200;
+	}
+
+	
 
 	cout << "The current values in this array are: ";
 	for (unsigned int i = 0; i < size(values); i++) {//iterates through array and prints out each value
@@ -28,19 +34,19 @@ int main(int argc, char** argv[]) {//these parameters inside main seem to be use
 	//therefore the loop runs the length of the given array -1
 	//there are most definetly better ways to optimize the algorithm to stop when it is sorted but this is the simplest way I could think of stopping it
 
-		sortArray(values, buffer, arraysize);
+		sortArray(values, arraysize);
 
 	}
 	
-	for (int j = 0; j < 11; j++)//iterating through the array one last time
+	for (int j = 0; j < arraysize; j++)//iterating through the array one last time
 		cout << values[j] << " ";//printing out the now sorted values
 	cout << endl;
 	return 0;
 }
 
-void sortArray(int  values[], int& buffer, int arraysize)
-{
-	for (unsigned int i = 0; i < arraysize - 1; i++) {//loop runs as long as the length of the array -1 to avoid overflow
+void sortArray(int  values[], int arraysize){
+	int buffer = 0;//integer used for temporarily storing a value of a number in the array
+	for (int i = 0; i < arraysize - 1; i++) {//loop runs as long as the length of the array -1 to avoid overflow
 		if (values[i] > values[i + 1]) {//if a number in the array is larger than the next number
 			buffer = values[i];//sets the buffer equal to the larger number
 			values[i] = values[i + 1];//sets the bigger value equal to the smaller value
--- my sorting alg/Source.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void sortArray(int  values[11], int& buffer);
